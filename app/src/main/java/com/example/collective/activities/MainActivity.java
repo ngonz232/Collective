60d7553d0668ee54f842f094f10f01f417ecb005
ngonz232 <ngonz232@fiu.edu>
2021-08-02 16:01:49 -0400

    Created main fragment view and navigation methods with the bottombar

--- app/src/main/java/com/example/collective/activities/MainActivity.java
+++ app/src/main/java/com/example/collective/activities/MainActivity.java
@@ -1,128 +1,87 @@
 package com.example.collective.activities;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.MenuItemCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.example.collective.EndlessRecyclerViewScrollListener;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import com.example.collective.R;
-import com.example.collective.Utils;
-import com.example.collective.adapters.eventsAdapter;
 import com.example.collective.databinding.ActivityMainBinding;
-import com.example.collective.models.Event;
-import com.parse.ParseQuery;
+import com.example.collective.fragments.CreateEvent;
+import com.example.collective.fragments.HomeFeed;
+import com.example.collective.fragments.MapsFragment;
+import com.example.collective.fragments.ProfileFragment;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
 
 public class MainActivity extends AppCompatActivity {
-private static final String TAG = "MainActivity";
-private eventsAdapter adapter;
 
-private List<Event> allEvents;
+    // Initializing binding
+    private ActivityMainBinding binding;
 
-private ActivityMainBinding binding;
-
+    // Creating and inflating view
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        RecyclerView rvEvents = binding.rvEvents;
+        // Initializing FragmentManager
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
-        allEvents = new ArrayList<>();
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        adapter = new eventsAdapter(allEvents, this);
-        rvEvents.setLayoutManager(layoutManager);
-        rvEvents.setAdapter(adapter);
-        Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        queryEvents(0);
+        // Initializing variables for each fragment
+        final Fragment fragment1 = new HomeFeed();
+        final Fragment fragment2 = new CreateEvent();
+        final Fragment fragment3 = new MapsFragment();
+        final Fragment fragment4 = new ProfileFragment();
 
-        binding.swipeContainer.setOnRefreshListener(() -> {
-            allEvents.clear();
-            queryEvents(0);
-            binding.swipeContainer.setRefreshing(false);
-        });
-        binding.swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright);
+        // Binding and setting a listener on the bottom navigation bar for the tab select
+        binding.bottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
 
-        binding.rvEvents.addOnScrollListener(new EndlessRecyclerViewScrollListener(layoutManager) {
+            /* Switch case statement checking which tab is select and breaking to pass it to the
+            fragment manager to inflate that fragment
+             */
             @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                queryEvents(allEvents.size());
-            }
-
-        });
-
-    }
-
-    public void queryEvents(int skip) {
-        ParseQuery<Event> query = ParseQuery.getQuery(Event.class);
-        query.addDescendingOrder("createdAt");
-        query.setSkip(skip);
-        query.setLimit(20);
-        query.findInBackground((events, e) -> {
-
-            if (e != null) {
-                Log.e(TAG, "Issue with getting events", e);
-                return;
-            }
-
-            // Save received events to list and notify adapter of new data
-            allEvents.addAll(events);
-            adapter.notifyDataSetChanged();
-
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
-        ImageView icon = searchView.findViewById(androidx.appcompat.R.id.search_button);
-        ImageView icon2 = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
-        icon.setColorFilter(Color.WHITE);
-        icon2.setColorFilter(Color.WHITE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchView.clearFocus();
-                if(allEvents.contains(query)){
-                    adapter.getFilter().filter(query);
-                } else {
-                    Toast.makeText(MainActivity.this, "No Match found",Toast.LENGTH_LONG).show();
+            public void onTabSelected(int i, @Nullable AnimatedBottomBar.Tab tab, int i1, @NotNull AnimatedBottomBar.Tab tab1) {
+                Fragment fragment;
+                String title = tab1.getTitle();
+                switch (title) {
+                    case "Home":
+                        fragment = fragment1;
+                        break;
+                    case "Publish":
+                        fragment = fragment2;
+                        break;
+                    case "Map":
+                        fragment = fragment3;
+                        break;
+                    case "Profile":
+                        fragment = fragment4;
+                        break;
+                    default:
+                        fragment = fragment1;
+                        break;
                 }
-                return false;
+                // Beginning transaction to show fragment and setting transition animations
+                fragmentManager.beginTransaction().setCustomAnimations(R.anim.fade_in,R.anim.slide_out).replace(R.id.fragmentContainer, fragment).commit();
 
             }
 
+            /* Empty required method for onTabReselected, but we are not taking actions on
+            tab reselection
+             */
             @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
+            public void onTabReselected(int i, @NotNull AnimatedBottomBar.Tab tab) {}
+
+            });
+            // Set default fragment
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment1).commit();
+        }
+
+
+
     }
-
-   }
-
-
